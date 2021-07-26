3e20cb162c667c5a9140fed4e4e3aa261a22a16a
Centeno448 <diego.centeno@udla.edu.ec>
2021-07-26 18:41:19 -0500

    resume base design

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,7 +1,43 @@
+import './Resume.css';
+
 const Resume = () => {
   return (
     <>
-      <p>Resume</p>
+      <div className="row justify-content-center ">
+        <div className="col-6 text-center section-title-container">
+          <h5 className="section-title">Experience</h5>
+        </div>
+      </div>
+      <div className="row justify-content-center ">
+        <div className="col-6">
+          <div className="content-title float-md-start">
+            Systems Intern - UDLA, Quito
+          </div>
+          <div className="content-year float-md-end">10/2019 - 10/2020</div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-6 content-tasks-container">
+          <ul>
+            <li className="content-task">Analisis</li>
+            <li>Desarrollo</li>
+            <li>Pruebas</li>
+          </ul>
+        </div>
+      </div>
+      <div className="row justify-content-center ">
+        <div className="col-6 text-center section-title-container">
+          <h5 className="section-title">Education</h5>
+        </div>
+      </div>
+      <div className="row justify-content-center ">
+        <div className="col-6">
+          <div className="content-title float-md-start">
+            Systems Engineer - UDLA, Quito
+          </div>
+          <div className="content-year float-md-end">09/2016 - 08/2021</div>
+        </div>
+      </div>
     </>
   );
 };
