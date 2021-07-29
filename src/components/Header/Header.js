fff0604fced8ae316e8f5bb22bbf655d52b1e2c7
Centeno448 <diego.centeno@udla.edu.ec>
2021-07-29 16:40:15 -0500

    responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,29 +4,45 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <>
-      <nav className="navbar bg-light">
+      <nav className="navbar navbar-light navbar-expand-md bg-light">
         <div className="container-fluid">
           <span className="navbar-brand mb-0 h1 nav-title">
             Diego Centeno
             <span className="nav-subtitle">Fullstack Developer</span>
           </span>
-          <ul className="nav justify-content-end">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/resume">
-                Resume
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/projects">
-                Projects
-              </Link>
-            </li>
-          </ul>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navBarContent"
+            aria-controls="navBarContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navBarContent"
+          >
+            <ul className="navbar-nav justify-content-end">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/resume">
+                  Resume
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/projects">
+                  Projects
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
